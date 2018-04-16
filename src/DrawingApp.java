1a8f0974a1b4c2ec4daccbee2fb047cbb42bcbfb
Iulia <jalbapunctiulia@gmail.com>
2018-04-16 22:56:30 +0300

    yelooooo
    Signed-off-by: Iulia <jalbapunctiulia@gmail.com>

---
+++ src/DrawingApp.java
@@ -0,0 +1,16 @@
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle t = (Triangle) context.getBean("triangle");
+
+        t.draw();
+
+    }
+}
