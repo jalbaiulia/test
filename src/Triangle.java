c03fb8348edb27f2d8d4822fbe6dbe308a685de4
Iulia <jalbapunctiulia@gmail.com>
2018-04-18 01:11:22 +0300

    next
    Signed-off-by: Iulia <jalbapunctiulia@gmail.com>

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,18 +1,78 @@
 public class Triangle {
 
     private String type;
+    private int height;
 
-    public String getType() {
-        return type;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
+
+
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
     }
 
     public void setType(String type) {
         this.type = type;
     }
 
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    // constructor injection
+    public Triangle(String type){
+        this.type = type;
+    }
+
+    public Triangle(int height){
+        this.height = height;
+
+    }
+
+    public Triangle(int height, String type){
+        this.height = height;
+        this.type = type;
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+
+//    // method injection
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+
     public void draw(){
 
-        System.out.println("Triangle drawn "+ getType());
+        System.out.println("Triangle drawn "+ getType() + " " + getHeight() + " " + pointA + pointB + pointC);
     }
 
 }
